53268651397c3bc236633c382517272c4d2fbb71
gonglijing <gong@kdisi.com>
2026-02-06 23:29:57 +0800

    feat: add go-plugin entrypoints for northbound adapters

---
+++ src/northbound-mqtt/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gonglijing/xunjiFsu/internal/northbound"
+	"github.com/gonglijing/xunjiFsu/plugin_north/adapter"
+	plugin "github.com/hashicorp/go-plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: northbound.NorthboundHandshake,
+		Plugins: map[string]plugin.Plugin{
+			northbound.NorthboundPluginName: &northbound.NorthboundPlugin{Impl: adapter.NewMQTTAdapter()},
+		},
+	})
+}
